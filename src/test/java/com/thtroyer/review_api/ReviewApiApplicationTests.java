d04a597f39759fd644d08d2cb0b566c49df9ed1b
Tom Troyer <tom.troyer@gmail.com>
2019-05-18 15:21:46 -0400

    Initial project state

---
+++ src/test/java/com/thtroyer/review_api/ReviewApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thtroyer.review_api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReviewApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
