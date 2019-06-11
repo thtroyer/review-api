9dc5c1ee62150f441b64ec2002ebfc667116c5bf
Tom Troyer <tom.troyer@gmail.com>
2019-06-11 14:54:13 -0400

    Starting UI work (and learning react).
    Fetching categories for display.

--- src/main/java/com/thtroyer/review_api/controllers/CategoryController.java
+++ src/main/java/com/thtroyer/review_api/controllers/CategoryController.java
@@ -2,13 +2,11 @@ package com.thtroyer.review_api.controllers;
 
 import com.thtroyer.review_api.model.Category;
 import com.thtroyer.review_api.repository.CategoryRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class CategoryController {
     private final CategoryRepository categoryRepository;
--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class CategoryView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            categories: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:8080/category")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    this.setState({
+                        isLoaded: true,
+                        categories: result
+                    });
+                },
+            )
+    }
+
+    render() {
+        const { error, isLoaded, categories } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div>
+                    <h2>Categories</h2>
+                    <ul>
+                        {categories.map(category => (
+                            <li key={category.id}>
+                                {category.id} {category.name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            );
+        }
+    }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <div>
+              <CategoryView/>
+          </div>
+      </div>
   );
 }
 
